4b929bb60331e6126dc743fb71c83ef8da09281b
xlg-JUST <935003328@qq.com>
2022-04-01 02:17:12 +0800

    CrossDone

--- Models/Ours.py
+++ Models/Ours.py
@@ -63,67 +63,79 @@ if __name__ == '__main__':
     files = os.listdir(filepath)
 
     # 混合
-    x_train, x_test, y_train, y_test = mix_project(filepath, testsize=0.1)
-    x_train, x_test = str2list(x_train), str2list(x_test)
-    x_train, x_test = word2index(x_train, vocab), word2index(x_test, vocab)
-    x_train, x_test = pad(x_train, maxlen=50), pad(x_test, maxlen=50)
+    # x_train, x_test, y_train, y_test = mix_project(filepath, testsize=0.1)
+    # x_train, x_test = str2list(x_train), str2list(x_test)
+    # x_train, x_test = word2index(x_train, vocab), word2index(x_test, vocab)
+    # x_train, x_test = pad(x_train, maxlen=50), pad(x_test, maxlen=50)
+    # label_names = ['Non-SATD', 'Design', 'Requirement']
+    # metric = ['Precision', 'Recall', 'F1', 'Gmean', 'AUC']
+    # de_pf, re_pf = [], []
+    # for i in range(20):
+    #     best_f1, best_result = 0, []
+    #     cur_model = MyLCM(sen_len=50, vocab_size=len(vocab), wvdim=100, hidden_size=50, class_weight={0: 1, 1: 2, 2: 3})
+    #     for _ in range(5):
+    #         cur_model.fit(x_train, y_train, epoch=1)
+    #         y_pred, y_score = cur_model.predict(x_test)
+    #         result = performence(y_test, y_pred, y_score, label_names, report=True)
+    #         f1 = sum(result[1:, 2].reshape(-1))
+    #         if f1 > best_f1:
+    #             best_f1, best_result = f1, result
+    #     de_pf.append(best_result[1, :]), re_pf.append(best_result[-1, :])
+    # de_pf, re_pf = np.vstack(de_pf), np.vstack(re_pf)
+    # de_df = pd.DataFrame(de_pf, columns=metric)  # 只需要 设计债和需求债
+    # re_df = pd.DataFrame(re_pf, columns=metric)  # 只需要 设计债和需求债
+    # de_df.to_csv(save_path+'Mix_project_Design.csv')
+    # re_df.to_csv(save_path+'Mix_project_Requirement.csv')
+
+    # # 跨项目
+    design, requirement = [], []
     label_names = ['Non-SATD', 'Design', 'Requirement']
     metric = ['Precision', 'Recall', 'F1', 'Gmean', 'AUC']
-    de_pf, re_pf = [], []
-    for i in range(20):
-        best_f1, best_result = 0, []
+    for file in files:
+        x_train, x_test, y_train, y_test = cross_project(filepath, file)
+        x_train, x_test = str2list(x_train), str2list(x_test)
+        x_train, x_test = word2index(x_train, vocab), word2index(x_test, vocab)
+        x_train, x_test = pad(x_train, maxlen=50), pad(x_test, maxlen=50)
         cur_model = MyLCM(sen_len=50, vocab_size=len(vocab), wvdim=100, hidden_size=50, class_weight={0: 1, 1: 2, 2: 3})
+        best_f1, best_result = 0, []
         for _ in range(5):
             cur_model.fit(x_train, y_train, epoch=1)
             y_pred, y_score = cur_model.predict(x_test)
-            result = performence(y_test, y_pred, y_score, label_names, report=True)
+            result = performence(y_test, y_pred, y_score, label_names)
             f1 = sum(result[1:, 2].reshape(-1))
             if f1 > best_f1:
                 best_f1, best_result = f1, result
-        de_pf.append(best_result[1, :]), re_pf.append(best_result[-1, :])
-    de_pf, re_pf = np.vstack(de_pf), np.vstack(re_pf)
-    de_df = pd.DataFrame(de_pf, columns=metric)  # 只需要 设计债和需求债
-    re_df = pd.DataFrame(re_pf, columns=metric)  # 只需要 设计债和需求债
-    de_df.to_csv(save_path+'Mix_project_Design.csv')
-    re_df.to_csv(save_path+'Mix_project_Requirement.csv')
-
-    # # 跨项目
-    # design, requirement = [], []
-    # label_names = ['Non-SATD', 'Design', 'Requirement']
-    # metric = ['Precision', 'Recall', 'F1', 'Gmean', 'AUC']
-    # for file in files:
-    #     x_train, x_test, y_train, y_test = cross_project(filepath, file)
-    #     x_train, x_test = word2index(x_train, vocab), word2index(x_test, vocab)
-    #     x_train, x_test = pad(x_train, maxlen=50), pad(x_test, maxlen=50)
-    #     Model = MyLCM(sen_len=50, vocab_size=len(vocab), wvdim=100, hidden_size=50, class_weight={0: 1, 1: 2, 2: 3})
-    #     Model.fit(x_train, y_train, epoch=1)
-    #     y_pred, y_score = Model.predict(x_test)
-    #     result = performence(y_test, y_pred, y_score, label_names)
-    #     design.append(result[1, :].reshape(-1)), requirement.append(result[2, :].reshape(-1))
-    # design, requirement = np.vstack(design), np.vstack(requirement)
-    # de_df = pd.DataFrame(design, index=files, columns=metric)
-    # re_df = pd.DataFrame(requirement, index=files, columns=metric)
-    # de_df.to_csv(save_path+'CrossProject:Design.csv')
-    # re_df.to_csv(save_path+'CrossProject:Requirement.csv')
+        design.append(best_result[1, :].reshape(-1)), requirement.append(best_result[2, :].reshape(-1))
+    design, requirement = np.vstack(design), np.vstack(requirement)
+    de_df = pd.DataFrame(design, index=files, columns=metric)
+    re_df = pd.DataFrame(requirement, index=files, columns=metric)
+    de_df.to_csv(save_path+'CrossProject_Design.csv')
+    re_df.to_csv(save_path+'CrossProject_Requirement.csv')
     #
     # # 项目内
-    # design, requirement = [], []
-    # label_names = ['Non-SATD', 'Design', 'Requirement']
-    # metric = ['Precision', 'Recall', 'F1', 'Gmean', 'AUC']
-    # for file in files:
-    #     x_train, x_test, y_train, y_test = within_project(filepath, file, testsize=0.1)
-    #     x_train, x_test = word2index(x_train, vocab), word2index(x_test, vocab)
-    #     x_train, x_test = pad(x_train, maxlen=50), pad(x_test, maxlen=50)
-    #     Model = MyLCM(sen_len=50, vocab_size=len(vocab), wvdim=100, hidden_size=50, class_weight={0: 1, 1: 2, 2: 3})
-    #     Model.fit(x_train, y_train, epoch=3)
-    #     y_pred, y_score = Model.predict(x_test)
-    #     result = performence(y_test, y_pred, y_score, label_names)
-    #     design.append(result[1, :].reshape(-1)), requirement.append(result[2, :].reshape(-1))
-    # design, requirement = np.vstack(design), np.vstack(requirement)
-    # de_df = pd.DataFrame(design, index=files, columns=metric)
-    # re_df = pd.DataFrame(requirement, index=files, columns=metric)
-    # de_df.to_csv(save_path + 'WithinProject:Design.csv')
-    # re_df.to_csv(save_path + 'WithinProject:Requirement.csv')
+    design, requirement = [], []
+    label_names = ['Non-SATD', 'Design', 'Requirement']
+    metric = ['Precision', 'Recall', 'F1', 'Gmean', 'AUC']
+    for file in files:
+        x_train, x_test, y_train, y_test = within_project(filepath, file, testsize=0.1)
+        x_train, x_test = str2list(x_train), str2list(x_test)
+        x_train, x_test = word2index(x_train, vocab), word2index(x_test, vocab)
+        x_train, x_test = pad(x_train, maxlen=50), pad(x_test, maxlen=50)
+        best_f1, best_result = 0, []
+        for _ in range(5):
+            cur_model = MyLCM(sen_len=50, vocab_size=len(vocab), wvdim=100, hidden_size=50, class_weight={0: 1, 1: 2, 2: 3})
+            cur_model.fit(x_train, y_train, epoch=3)
+            y_pred, y_score = cur_model.predict(x_test)
+            result = performence(y_test, y_pred, y_score, label_names)
+            f1 = sum(result[1:, 2].reshape(-1))
+            if f1 > best_f1:
+                best_f1, best_result = f1, result
+        design.append(best_result[1, :].reshape(-1)), requirement.append(best_result[2, :].reshape(-1))
+    design, requirement = np.vstack(design), np.vstack(requirement)
+    de_df = pd.DataFrame(design, index=files, columns=metric)
+    re_df = pd.DataFrame(requirement, index=files, columns=metric)
+    de_df.to_csv(save_path + 'WithinProject_Design.csv')
+    re_df.to_csv(save_path + 'WithinProject_Requirement.csv')
 
 
 
